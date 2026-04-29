808515b7a2c015c2aaf97bf06bc9be72a87b3be2
= <tagalomalo@gmail.com>
2026-04-29 23:01:00 +0700

    I forgot to delete the css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,7 +43,7 @@ onMounted(() => {
   <main class="bg-neutral-900">
     <section
       id="home"
-      class="relative min-h-[120vh] flex justify-center overflow-hidden bg-hero-pattern after:content-[''] after:absolute after:bottom-0 after:left-0 after:right-0 after:h-72 after:bg-linear-to-b after:from-transparent after:to-neutral-900 after:pointer-events-none after:z-20"
+      class="sect-anim relative min-h-[120vh] flex justify-center overflow-hidden bg-hero-pattern after:content-[''] after:absolute after:bottom-0 after:left-0 after:right-0 after:h-72 after:bg-linear-to-b after:from-transparent after:to-neutral-900 after:pointer-events-none after:z-20"
     >
       <img
         src="@/assets/background.png"
@@ -68,53 +68,6 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.hero {
-  min-height: 125vh;
-  position: relative;
-  overflow: hidden;
-  background:
-    radial-gradient(ellipse at 55% 60%, rgba(255, 255, 255, 0.25) 4%, transparent 60%),
-    radial-gradient(ellipse at 55% 40%, rgba(101, 22, 161, 0.19) 0%, transparent 50%),
-    rgb(23, 23, 23);
-}
-
-.hero > img {
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: center;
-  opacity: 0.12;
-  z-index: 0;
-}
-
-.hero > :not(img) {
-  position: relative;
-  z-index: 1;
-}
-
-.hero::after {
-  z-index: 2;
-  content: "";
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 280px;
-  background: linear-gradient(to bottom, transparent, rgb(23, 23, 23));
-  pointer-events: none;
-}
-
-.about,
-.projects,
-.contact {
-  min-height: 100vh;
-  background: rgb(23, 23, 23);
-  color: white;
-  padding: 12vh 18%;
-}
-
 .sect-anim {
   opacity: 0;
   transform: translateY(50px);
